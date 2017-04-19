1de0d0ac02df36720fb23e423de0e7f236a31ae8
Mathieu d'Aquin <mathieu.daquin@insight-centre.org>
2017-04-19 09:47:41 +0100

    clean up

--- conf/conf-generic.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$defaultendpoint = "http://kmi-web11.open.ac.uk:8080/openrdf-workbench/repositories/bigowlimTest/query";
-$defaultclass    = "http://xmlns.com/foaf/0.1/Person";
-$memberlistlimit = "100";
-$classlistlimit = 100;
-$piechartcolor  = "FF0000,FFFF00";
-$chartcolor = "FF0000|00FFFF|0000FF|FFFF00|00FF00|FF00FF";
-?>
